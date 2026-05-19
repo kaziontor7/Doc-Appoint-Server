e50e444920207cf9a6f8ce7c0dfad54ffba862b8
kaziontor7 <kaziontor7@gmail.com>
2026-05-19 16:42:49 +0600

    added all doc get

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
+app.use(express.json());
 dotenv.config();
 const port = process.env.PORT
 const uri = process.env.MONGODB_URI
@@ -19,12 +20,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("doc-appoint");
+    const appointCollection = db.collection("appoints");
+    app.get('/appoints', async (req, res) => {
+      const result = await appointCollection.find().toArray();
+      res.send(result);
+    });
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }run().catch(console.dir);
 
